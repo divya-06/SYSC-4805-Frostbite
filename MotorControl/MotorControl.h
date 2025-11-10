73666e03f81868615e2581a798eb2548b9da7a53
Saja Fawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2025-11-09 20:57:23 -0500

    Add MotorControl header file with motor functions

---
+++ MotorControl/MotorControl.h
@@ -0,0 +1,13 @@
+// MotorControl.h
+#pragma once
+#include <Arduino.h>
+#include "CytronMotorDriver.h"
+
+// Initialize motors
+void initMotors();
+
+// Movement helpers
+void moveForward(int v);
+void moveBackward(int v);
+void turnCW(int v);
+void stopCar();
