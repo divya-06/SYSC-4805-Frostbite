c081ead4c61a4c21e04a864f5009dd7bd48af437
Saja Fawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2025-11-12 23:43:29 -0500

    Simplify encoder header inclusion
    Removed conditional includes for Encoders.h and Encoder.h.

--- UnitTests/test_encoders.cpp
+++ UnitTests/test_encoders.cpp
@@ -1,14 +1,6 @@
 #include <ArduinoFake.h>
 #include <unity.h>
-
-// Support either Encoders.h or Encoder.h (student repo variance)
-#if __has_include("Encoders.h")
-  #include "Encoders.h"
-#elif __has_include("Encoder.h")
-  #include "Encoder.h"
-#else
-  #error "Include Encoders.h or Encoder.h in include path"
-#endif
+#include "Encoders.h"
 
 using namespace fakeit;
 
@@ -16,16 +8,13 @@ void setUp() { ArduinoFakeReset(); }
 
 void test_encoders_init_and_reset() {
   When(Method(ArduinoFake(), pinMode)).AlwaysReturn();
-  // Some repos use attachInterrupt(digitalPinToInterrupt(...), ...);
+
   When(OverloadedMethod(ArduinoFake(), attachInterrupt, void(int, voidFuncPtr, int))).AlwaysReturn();
 
   initEncoders();
   resetTicks();
   TEST_ASSERT_EQUAL(0, getRightTicks());
   TEST_ASSERT_EQUAL(0, getLeftTicks());
-
-  // NOTE: For deeper testing, expose a test hook in your module to increment ticks
-  // (e.g., a function that calls the internal ISR), then assert increments here.
 }
 
 int main(int, char**) {
