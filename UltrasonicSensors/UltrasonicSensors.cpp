8daf682c933cbfbe1fa0f9179fb97676d2c61caa
vaanuthanesh2003 <vaanathythaneskumar@cmail.carleton.ca>
2025-11-25 17:10:49 -0500

    Update UltrasonicSensors.cpp

--- UltrasonicSensors/UltrasonicSensors.cpp
+++ UltrasonicSensors/UltrasonicSensors.cpp
@@ -4,14 +4,17 @@
 // Pins
 #define TRIG1 7
 #define ECHO1 6
-#define TRIG2 4
-#define ECHO2 3
+#define TRIG2 5
+#define ECHO2 4
+#define TRIG3 3
+#define ECHO3 2
 
 static float dist1 = 0.0f;
 static float dist2 = 0.0f;
+static float dist3 = 0.0f;
 
 // Obstacle detection params
-const float OBSTACLE_THRESHOLD_CM = 15.0f;
+const float OBSTACLE_THRESHOLD_CM = 25.0f;
 const uint8_t OB_DETECT_COUNT_REQ = 2;
 static uint8_t obDetectCount = 0;
 
@@ -31,18 +34,22 @@ void initUltrasonic() {
   pinMode(ECHO1, INPUT);
   pinMode(TRIG2, OUTPUT);
   pinMode(ECHO2, INPUT);
+  pinMode(TRIG3, OUTPUT);
+  pinMode(ECHO3, INPUT);
 }
 
 void updateUltrasonic() {
   static unsigned long lastUS = 0;
-  if (millis() - lastUS >= 50) { // ~20 Hz
+  if (millis() - lastUS >= 10) { // ~20 Hz
     lastUS = millis();
     dist1 = getDistanceCM(TRIG1, ECHO1);
     dist2 = getDistanceCM(TRIG2, ECHO2);
+    dist3 = getDistanceCM(TRIG3, ECHO3);
 
     bool obstacleNow = false;
     if ((dist1 > 0 && dist1 <= OBSTACLE_THRESHOLD_CM) ||
-        (dist2 > 0 && dist2 <= OBSTACLE_THRESHOLD_CM)) {
+        (dist2 > 0 && dist2 <= OBSTACLE_THRESHOLD_CM) ||
+        (dist3 > 0 && dist3 <= OBSTACLE_THRESHOLD_CM)) {
       obstacleNow = true;
     }
 
