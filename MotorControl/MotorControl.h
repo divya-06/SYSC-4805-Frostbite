680cd6e3d67e871ad054a335b3fe19cd11756a67
Saja Fawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2025-11-25 17:25:42 -0500

    Add movement functions to MotorControl header
    Added movement functions for motor control.

--- MotorControl/MotorControl.h
+++ MotorControl/MotorControl.h
@@ -1,12 +1,11 @@
-// MotorControl.h
 #pragma once
 #include <Arduino.h>
 #include "CytronMotorDriver.h"
 
-// Initialize motors
 void initMotors();
 
-// Movement helpers
+ 
+// Movement
 void moveForward(int v);
 void moveBackward(int v);
 void turnCW(int v);
